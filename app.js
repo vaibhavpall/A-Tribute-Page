9d0b3f6d7b91362fc6a6a57c502e342fa550b08b
Vaibhav Pal <115584189+vaibhavpall@users.noreply.github.com>
2022-11-15 00:25:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+console.log('nikolatesla');
+localStorage.setItem('username');
+localStorage.setItem('email');
+localStorage.setItem('phone');
+localStorage.setItem('message');
+
+
+
+
+
+
